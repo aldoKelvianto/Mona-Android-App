d2166331dac833dc6d254ca1661a988eed00e4ac
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-03 22:35:38 +0700

    Add MainActivity

---
+++ app/src/main/java/com/example/mona/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.mona
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

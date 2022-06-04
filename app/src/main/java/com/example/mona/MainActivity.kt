fc2bb2c908cfed99606e29b192270bfec300489a
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-04 11:49:14 +0700

    Add Jetpack Compose

--- app/src/main/java/com/example/mona/MainActivity.kt
+++ app/src/main/java/com/example/mona/MainActivity.kt
@@ -1,10 +1,30 @@
 package com.example.mona
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : Activity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Greeting() {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Text(text = "Hello, world!")
+        Text(text = "Hello, world, but not really!")
     }
 }

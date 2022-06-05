c6d3e244f1566a9c47b07e11dc35dddc28466aa4
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-05 22:56:29 +0700

    Working on ItemCategory

---
+++ app/src/main/java/com/example/mona/compose/Color.kt
@@ -0,0 +1,8 @@
+package com.example.mona.compose
+
+import androidx.compose.ui.graphics.Color
+
+val Blue700 = Color(0xff1976d2)
+val LightBlue50 = Color(0xffe1f5fe)
+
+val ItemCategoryGradient = listOf(LightBlue50, Blue700)
--- app/src/main/java/com/example/mona/compose/ItemCategory.kt
+++ app/src/main/java/com/example/mona/compose/ItemCategory.kt
@@ -1,21 +1,67 @@
 package com.example.mona.compose
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mona.R
+
+fun Modifier.gradientBackground(
+    colors: List<Color>
+) = background(
+    Brush.radialGradient(
+        colors = colors,
+        radius = 250f,
+        center = Offset(120f, 50f)
+    )
+)
 
 @Preview
 @Composable
 fun ItemCategory() {
-    Box(
-        modifier = Modifier
-            .height(100.dp)
-            .width(100.dp)
+    Card(
+        elevation = 10.dp,
+        shape = RoundedCornerShape(8.dp)
     ) {
+        Column(
+            modifier = Modifier
+                .gradientBackground(ItemCategoryGradient)
+        ) {
+            Text(
+                text = "Sayur dan buah",
+                modifier = Modifier.padding(12.dp),
+                color = Blue700,
+                textAlign = TextAlign.Center
+            )
+            Box(modifier = Modifier.width(100.dp).padding(8.dp), contentAlignment = Alignment.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_g1_vegs_a),
+                    modifier = Modifier
+                        .offset(x = (-10).dp)
+                        .size(60.dp),
+                    contentDescription = "desc"
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_g1_vegs_b),
+                    modifier = Modifier
+                        .offset(x = 10.dp)
+                        .size(40.dp),
+                    contentDescription = "desc"
+                )
+            }
 
+        }
     }
 }

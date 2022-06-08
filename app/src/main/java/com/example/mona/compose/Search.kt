2f1abdcb2843269913cf7906c3f724fe39e2c1c9
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-08 19:52:35 +0700

    Refactor Search composable

--- app/src/main/java/com/example/mona/compose/Search.kt
+++ app/src/main/java/com/example/mona/compose/Search.kt
@@ -1,16 +1,14 @@
 package com.example.mona.compose
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,27 +16,32 @@ import androidx.compose.ui.unit.dp
 import com.example.mona.R
 
 @Composable
-fun Search(hint: String) {
+fun RowScope.Search(hint: String) {
     Row(
         modifier = Modifier
             .background(
-                color = Color.White, shape = RoundedCornerShape(4.dp)
+                color = Color.White,
+                shape = RoundedCornerShape(4.dp),
             )
-            .fillMaxWidth(),
+            .clip(RoundedCornerShape(8.dp))
+            .weight(1f)
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_search_outline_24),
             contentDescription = "Search icon"
         )
+        Spacer(modifier = Modifier.size(4.dp))
         Text(text = hint, color = Color.Blue)
     }
-
 }
 
 @Preview(showBackground = true, backgroundColor = 0xff212121, widthDp = 320, heightDp = 640)
 @Composable
 fun PreviewSearch() {
     val hint = "Cari produk disini"
-    Search(hint)
+    Row {
+        Search(hint)
+    }
 }
--- app/src/main/java/com/example/mona/compose/SectionSearchFavorite.kt
+++ app/src/main/java/com/example/mona/compose/SectionSearchFavorite.kt
@@ -1,19 +1,14 @@
 package com.example.mona.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.mona.R
 
 @Preview(showBackground = true, backgroundColor = 0xff212121, widthDp = 320, heightDp = 640)
 @Composable
@@ -24,28 +19,10 @@ fun PreviewSectionSearchFavorite() {
 @Composable
 fun SectionSearchFavorite() {
     Row(
-        modifier = Modifier.fillMaxWidth()
-            ,
+        modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
-            modifier = Modifier
-                .background(
-                    color = Color.White,
-                    shape = RoundedCornerShape(4.dp),
-                )
-                .clip(RoundedCornerShape(8.dp))
-                .weight(1f)
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_search_outline_24),
-                contentDescription = "Search icon"
-            )
-            Spacer(modifier = Modifier.size(4.dp))
-            Text(text = "Cari produk disini", color = Color.Blue)
-        }
+        Search(hint = "Cari produk disini")
         Spacer(modifier = Modifier.size(8.dp))
         Favorite()
     }

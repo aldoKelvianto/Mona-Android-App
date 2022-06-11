5482a4e6e5502b3485b802304265bfdfb2d37147
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-11 19:16:45 +0700

    Trying to use constraintlayout 2

--- app/src/main/java/com/example/mona/compose/SectionTop.kt
+++ app/src/main/java/com/example/mona/compose/SectionTop.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.mona.R
 
 @Composable
 fun SectionTop() {
     ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
-        val (column, spacer, sectionBanner) = createRefs()
+        val (column, spacer, newSpacer, sectionBanner) = createRefs()
 
         Column(modifier = Modifier
             .constrainAs(column) {
@@ -22,7 +26,6 @@ fun SectionTop() {
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
-            .background(Color.Red)
             .zIndex(1f)) {
             SectionLocation()
             SectionGreeting()
@@ -33,10 +36,25 @@ fun SectionTop() {
             )
             SectionSearchFavorite()
         }
-
-        Spacer(modifier = Modifier
+        val brush = Brush.radialGradient(
+            colors = HeaderColors,
+            radius = 550f,
+            center = Offset(766f, 10f)
+        )
+        Spacer(
+            modifier = Modifier
+                .constrainAs(newSpacer) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .background(brush)
+                .size(100.dp)
+                .zIndex(0f)
+        )
+        Column(modifier = Modifier
             .constrainAs(spacer) {
-                top.linkTo(column.bottom)
+                top.linkTo(parent.top)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
@@ -44,7 +62,12 @@ fun SectionTop() {
             .background(
                 Color.Green
             )
-            .zIndex(0f))
+            .zIndex(2f)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_g8_book_b),
+                contentDescription = "book"
+            )
+        }
 
         SectionBanner(
             Modifier

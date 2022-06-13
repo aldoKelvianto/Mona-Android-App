18a018875fdc72fc02d1708e41e3739482dd1dc5
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-13 13:06:17 +0700

    Use constraintlayout for address

--- app/src/main/java/com/example/mona/compose/SectionLocation.kt
+++ app/src/main/java/com/example/mona/compose/SectionLocation.kt
@@ -14,27 +14,41 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.mona.R
 
 @Composable
 fun SectionLocation() {
-    Row(
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 12.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(vertical = 8.dp, horizontal = 12.dp)
     ) {
-        Text(text = "Mondstadt", color = Color.White, fontWeight = FontWeight.Bold)
+        val (text, chevron) = createRefs()
+        Text(
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+            },
+            text = "Mondstadt",
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
         Icon(
+            modifier = Modifier
+                .constrainAs(chevron) {
+                    top.linkTo(parent.top)
+                    start.linkTo(text.end)
+                    bottom.linkTo(parent.bottom)
+                }
+                .rotate(90f),
             painter = painterResource(id = R.drawable.ic_chevron_outline_24),
             contentDescription = "Location icon",
-            modifier = Modifier.rotate(90f),
             tint = Color.White
         )
     }
-
 }
-
 
 @Preview(backgroundColor = 0xff212121, showBackground = true, widthDp = 320, heightDp = 640)
 @Composable

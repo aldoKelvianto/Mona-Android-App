463438ad8a85cbd4f49e299946ed2c6ecfda26dd
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-13 14:00:12 +0700

    Decoupled constraint

--- app/src/main/java/com/example/mona/compose/SectionLocation.kt
+++ app/src/main/java/com/example/mona/compose/SectionLocation.kt
@@ -1,12 +1,11 @@
 package com.example.mona.compose
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -15,46 +14,62 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.layoutId
 import com.example.mona.R
 
 @Composable
 fun SectionLocation() {
-    ConstraintLayout(
+    BoxWithConstraints(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp, horizontal = 12.dp)
     ) {
-        val (text, chevron, distance, point) = createRefs()
-        Text(
-            modifier = Modifier.constrainAs(text) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-                bottom.linkTo(parent.bottom)
-            },
-            text = "Mondstadt",
-            color = Color.White,
-            fontWeight = FontWeight.Bold
-        )
-        Icon(
-            modifier = Modifier
-                .constrainAs(chevron) {
-                    top.linkTo(parent.top)
-                    start.linkTo(text.end)
-                    bottom.linkTo(parent.bottom)
-                }
-                .rotate(90f),
-            painter = painterResource(id = R.drawable.ic_chevron_outline_24),
-            contentDescription = "Location icon",
-            tint = Color.White
-        )
-        Distance(
-            modifier = Modifier
-                .constrainAs(distance) {
-                    top.linkTo(parent.top)
-                    start.linkTo(chevron.end)
-                    bottom.linkTo(parent.bottom)
-                }
-        )
+        ConstraintLayout(
+            sectionLocationConstraint()
+        ) {
+            Text(
+                modifier = Modifier.layoutId("text"),
+                text = "Mondstadt",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Icon(
+                modifier = Modifier
+                    .layoutId("chevron")
+                    .rotate(90f),
+                painter = painterResource(id = R.drawable.ic_chevron_outline_24),
+                contentDescription = "Location icon",
+                tint = Color.White
+            )
+            Distance(
+                modifier = Modifier.layoutId("distance")
+            )
+        }
+    }
+}
+
+private fun sectionLocationConstraint() = ConstraintSet {
+    // At the time of writing, we can't use `createRefs`
+    // val (text, chevron, distance, point) = createRefs()
+    val text = createRefFor("text")
+    val chevron = createRefFor("chevron")
+    val distance = createRefFor("distance")
+
+    constrain(text) {
+        top.linkTo(parent.top)
+        start.linkTo(parent.start)
+        bottom.linkTo(parent.bottom)
+    }
+    constrain(chevron) {
+        top.linkTo(parent.top)
+        start.linkTo(text.end)
+        bottom.linkTo(parent.bottom)
+    }
+    constrain(distance) {
+        top.linkTo(parent.top)
+        start.linkTo(chevron.end)
+        bottom.linkTo(parent.bottom)
     }
 }
 

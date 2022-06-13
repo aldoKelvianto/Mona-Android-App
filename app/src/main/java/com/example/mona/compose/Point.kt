a99f29e5da611e6569e658cf55fbda3eef880d0c
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-13 13:21:31 +0700

    Add point composable

---
+++ app/src/main/java/com/example/mona/compose/Point.kt
@@ -0,0 +1,48 @@
+package com.example.mona.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mona.R
+import com.example.mona.theme.Grey900
+
+@Composable
+fun Point() {
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.White)
+            .padding(4.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_mora),
+                contentDescription = "Mora icon",
+                modifier = Modifier.size(24.dp)
+            )
+            Text(text = "100", style = TextStyle(Grey900))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPoint() {
+    Point()
+}

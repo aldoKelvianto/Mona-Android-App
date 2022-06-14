4e0e3cfb5f71eac4ae70c38f3a1451af0707bcb8
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-06-14 18:34:20 +0700

    Remove unused assets

--- app/src/main/java/com/example/mona/MainActivity.kt
+++ app/src/main/java/com/example/mona/MainActivity.kt
@@ -3,22 +3,14 @@ package com.example.mona
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
-import com.example.mona.compose.PreviewScreenHome
 import com.example.mona.compose.ScreenHome
-import com.example.mona.compose.ThemeMona
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // This app draws behind the system bars, so we want to handle fitting system windows
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
         setContent {
-            ThemeMona {
-                ScreenHome()
-            }
+            ScreenHome()
         }
     }
 }

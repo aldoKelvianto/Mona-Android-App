e4610c62631b353326300825dba937929a7d30ec
aldokelvianto <aldo.wachyudi@gmail.com>
2022-08-07 21:42:13 +0700

    Add base64 test

---
+++ app/src/test/java/com/example/mona/AndroidBase64Test.kt
@@ -0,0 +1,34 @@
+package com.example.mona
+
+import android.util.Base64
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+// To avoid "Method encodeToString in android.util.Base64 not mocked." error,
+// use robolectric or use emulator/test device.
+@RunWith(RobolectricTestRunner::class)
+class AndroidBase64Test {
+
+    @Test
+    fun `encode test`() {
+        // Given
+        val message = "hello?world".toByteArray()
+        // When
+        val outputPadding = Base64.encodeToString(message, Base64.DEFAULT)
+        val outputNoPadding = Base64.encodeToString(message, Base64.NO_PADDING)
+        val outputUrlSafe = Base64.encodeToString(message, Base64.URL_SAFE)
+        val outputNoClose = Base64.encodeToString(message, Base64.NO_CLOSE)
+        val outputNoWrap = Base64.encodeToString(message, Base64.NO_WRAP)
+        val outputCrlf = Base64.encodeToString(message, Base64.CRLF)
+
+        // Then
+        assertEquals("aGVsbG8/d29ybGQ=\n", outputPadding)
+        assertEquals("aGVsbG8/d29ybGQ\n", outputNoPadding)
+        assertEquals("aGVsbG8_d29ybGQ=\n", outputUrlSafe)
+        assertEquals("aGVsbG8/d29ybGQ=\n", outputNoClose)
+        assertEquals("aGVsbG8/d29ybGQ=", outputNoWrap)
+        assertEquals("aGVsbG8/d29ybGQ=\r\n", outputCrlf)
+    }
+}
---
+++ app/src/test/java/com/example/mona/JavaBase64Test.kt
@@ -0,0 +1,25 @@
+package com.example.mona
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.util.*
+
+class JavaBase64Test {
+
+    @Test
+    fun `encode test`() {
+        // Given
+        val message = "hello?world".toByteArray()
+        // When
+        val outputPadding = Base64.getEncoder().encodeToString(message)
+        val outputNoPadding = Base64.getEncoder().withoutPadding().encodeToString(message)
+        val outputUrlSafe = Base64.getUrlEncoder().encodeToString(message)
+        val outputUrlSafeNoPadding = Base64.getUrlEncoder().withoutPadding().encodeToString(message)
+
+        // Then
+        assertEquals("aGVsbG8/d29ybGQ=", outputPadding)
+        assertEquals("aGVsbG8/d29ybGQ", outputNoPadding)
+        assertEquals("aGVsbG8_d29ybGQ=", outputUrlSafe)
+        assertEquals("aGVsbG8_d29ybGQ", outputUrlSafeNoPadding)
+    }
+}

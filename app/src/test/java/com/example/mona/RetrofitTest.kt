2f7aeb348fda3caf45cfaa1b3a9e6ac9b83aac6c
aldoKelvianto <aldo.wachyudi@gmail.com>
2023-01-10 12:55:49 +0700

    Add postman test

--- app/src/test/java/com/example/mona/RetrofitTest.kt
+++ app/src/test/java/com/example/mona/RetrofitTest.kt
@@ -1,11 +1,14 @@
 package com.example.mona
 
-import junit.framework.Assert.assertTrue
+import com.google.gson.Gson
+import junit.framework.Assert.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface HttpBinService {
 
@@ -16,32 +19,85 @@ interface HttpBinService {
     suspend fun get500()
 }
 
+data class PostmanProfile(
+    val id: Int = -1
+)
+
+data class PostmanError(
+    val error: String = ""
+)
+
+interface PostmanService {
+
+    @GET("profile")
+    suspend fun get200(): PostmanProfile
+
+    @POST("profile")
+    suspend fun post500(): PostmanError
+}
+
 class RetrofitTest {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
+    private val httpBinRetrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://httpbin.org/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    private val service: HttpBinService = retrofit.create(HttpBinService::class.java)
+    private val httpBinService: HttpBinService = httpBinRetrofit.create(HttpBinService::class.java)
+
+    private val postmanRetrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://a1489da6-507a-49eb-931b-ab0ace8e6a95.mock.pstmn.io/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val postmanService: PostmanService = postmanRetrofit.create(PostmanService::class.java)
 
     @Test
-    fun testGet200() {
+    fun httpBin200() {
         runBlocking {
             val result = runCatching {
-                service.get200()
+                httpBinService.get200()
             }
             assertTrue(result.isSuccess)
         }
     }
 
     @Test
-    fun testGet500() {
+    fun httpBin500() {
         runBlocking {
             val result = runCatching {
-                service.get500()
+                httpBinService.get500()
             }
             assertTrue(result.isFailure)
+            val exception = result.exceptionOrNull()
+            assertNotNull(exception)
+            assertTrue(exception is HttpException)
+        }
+    }
+
+    @Test
+    fun postman200() {
+        runBlocking {
+            val result = kotlin.runCatching {
+                postmanService.get200()
+            }
+            assertTrue(result.isSuccess)
+            assertTrue(result.getOrThrow() == PostmanProfile(1))
+        }
+    }
+
+    @Test
+    fun postman500() {
+        runBlocking {
+            val result = kotlin.runCatching {
+                postmanService.post500()
+            }
+            assertTrue(result.isFailure)
+            val exception = result.exceptionOrNull() as HttpException
+            assertEquals(exception.code(), 500)
+            val errorBody = String(exception.response()!!.errorBody()!!.bytes())
+            val postmanError = Gson().fromJson(errorBody, PostmanError::class.java)
+            assertEquals(postmanError.error, "nginx not working")
         }
     }
 }
